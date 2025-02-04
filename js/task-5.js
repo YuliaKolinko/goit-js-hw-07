d4d3a0eb8b3082323c33c50be5c25ffc5da22291
Yulia <derevyanko.julia@gmail.com>
2025-02-04 09:57:40 +0200

    4-6 tasks

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const span = document.querySelector(".color");
+const title = document.querySelector(".text");
+const button = document.querySelector(".change-color");
+
+button.addEventListener("click", () => {
+  const NewColor = getRandomHexColor();
+  document.body.style.backgroundColor = NewColor;
+  span.textContent = NewColor;
+});
